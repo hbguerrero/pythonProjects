5fbb9b0e0773de0af0903d2fbbd07867ef3196f9
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 12:13:15 -0500

    Add files via upload

---
+++ helloWorld/thirdProgram.py
@@ -0,0 +1,19 @@
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "A colombian recipe"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
