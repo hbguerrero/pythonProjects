64624577ad00315dd21024b6ad48fc91029c870e
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2023-08-21 12:12:29 -0500

    Add files via upload

---
+++ helloWorld/secondProgram.py
@@ -0,0 +1,8 @@
+# Define the release and runtime integer variables below:
+release_year = 1977
+runtime = 1980
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = 6.8
+print(type(runtime))
+print(type(rating_out_of_10))
