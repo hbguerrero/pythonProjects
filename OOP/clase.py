62e1c041ce630a0e9c86ff5ebb1ccd9c64b923ca
hbguerrero <141659945+hbguerrero@users.noreply.github.com>
2024-09-16 06:22:39 -0500

    Add files via upload

---
+++ OOP/clase.py
@@ -0,0 +1,12 @@
+class Square:
+   def __init__(self,val):
+      self.val=val
+   def getVal(self):
+      return self.val*self.val
+ 
+ 
+class Add_Sub:
+   def add(self, a, b):
+      return a + b
+   def sub(self, a, b):
+      return a - b
---
+++ OOP/main.py
@@ -0,0 +1,11 @@
+import clase
+# creating object for Square class
+
+object1 = clase.Square(5)
+print(f"From class Square: {object1.getVal()}")
+# creating object for Add_Sub class
+
+object2 = clase.Add_Sub()
+
+print(f"From class Add_Sub: Addition {object2.add(2,3)}")
+print(f"From class Add_Sub: Subtraction {object2.sub(2,3)}")
